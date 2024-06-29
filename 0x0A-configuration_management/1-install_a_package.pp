4daa8211f1afd2db8a05dd5fff015ebf8a0bac63
mtmagic <mtmagic711@gmail.com>
2024-06-29 03:19:18 +0100

    task 1

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,10 @@
-# install flask from pip3
-package { 'flask':
+# 1-install_a_package.pp
+package { 'Flask':
   ensure   => '2.1.0',
+  provider => 'pip3',
+}
+
+package { 'Werkzeug':
+  ensure   => '2.1.1',
   provider => 'pip3',
 }
